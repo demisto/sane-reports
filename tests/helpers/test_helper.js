405d61d3b32df5dfdbaa314edaf89df29ecda038
bars92 <bar@demisto.com>
2019-05-05 11:48:11 +0200

    fix tests to match react 15

--- tests/helpers/test_helper.js
+++ tests/helpers/test_helper.js
@@ -3,7 +3,7 @@ import chai from 'chai';
 import * as constants from '../../src/constants/Constants';
 import * as TemplateProvider from '../../templates/templateProvider';
 import Enzyme, { mount, render, shallow } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import Adapter from 'enzyme-adapter-react-15';
 
 Enzyme.configure({ adapter: new Adapter() });
 const { assert, expect, should } = chai;
