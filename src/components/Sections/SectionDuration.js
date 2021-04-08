3d21772c34192e01029fd81a0be1d8ccfe79faac
yonatan-roth <yroth@paloaltonetworks.com>
2021-04-08 18:00:14 +0300

    Duration widget - support formats #33533

--- src/components/Sections/SectionDuration.js
+++ src/components/Sections/SectionDuration.js
@@ -7,16 +7,58 @@ function formatNumber(num) {
   return ('0' + num).slice(-2);
 }
 
+const createHeaders = (parts) => {
+  return (parts || []).map((part, ind) => {
+    return (
+      <React.Fragment key={'header' + part.header}>
+        <td className="time-unit">
+          {part.header}
+        </td>
+        {(ind + 1) < parts.length &&
+          <td />
+        }
+      </React.Fragment>
+    );
+  });
+};
+
+const createValues = (parts) => {
+  return (parts || []).map((part, ind) => {
+    return (
+      <React.Fragment key={'value' + part.header}>
+        <td className="part-header">{part.value}</td>
+        {(ind + 1) < parts.length &&
+          <td className="colon center aligned">:</td>
+        }
+      </React.Fragment>
+    );
+  });
+};
+
 const SectionDuration = ({ data, style, chartProperties, title, titleStyle }) => {
   let result = data.length > 0 && isArray(data[0].data) && data[0].data.length > 0 ? data[0].data[0] : 0;
-  let days = Math.floor(result / (3600 * 24));
-  result -= days * 3600 * 24;
-  let hours = Math.floor(result / 3600);
-  result -= hours * 3600;
-  let minutes = Math.floor(result / 60);
-  days = formatNumber(days);
-  hours = formatNumber(hours);
-  minutes = formatNumber(minutes);
+  const parts = (chartProperties && chartProperties.parts) ? chartProperties.parts : [];
+
+  if (parts.length === 0) {
+    const daysLabel = (chartProperties && chartProperties.daysLabel) || 'days';
+    let days = Math.floor(result / (3600 * 24));
+    result -= days * 3600 * 24;
+
+    const hoursLabel = (chartProperties && chartProperties.hoursLabel) || 'hours';
+    let hours = Math.floor(result / 3600);
+    result -= hours * 3600;
+
+    const minutesLabel = (chartProperties && chartProperties.minutesLabel) || 'minutes';
+    let minutes = Math.floor(result / 60);
+
+    days = formatNumber(days);
+    hours = formatNumber(hours);
+    minutes = formatNumber(minutes);
+
+    parts.push({ header: daysLabel, value: days });
+    parts.push({ header: hoursLabel, value: hours });
+    parts.push({ header: minutesLabel, value: minutes });
+  }
 
   return (
     <div className="section-duration" style={style}>
@@ -30,24 +72,10 @@ const SectionDuration = ({ data, style, chartProperties, title, titleStyle }) =>
             <table className="wrapper-table">
               <tbody>
                 <tr>
-                  <td className="days-number">{days}</td>
-                  <td className="colon center aligned">:</td>
-                  <td className="hours-number">{hours}</td>
-                  <td className="colon center aligned">:</td>
-                  <td className="minutes-number">{minutes}</td>
+                  {createValues(parts)}
                 </tr>
                 <tr>
-                  <td className="time-unit">
-                    {(chartProperties && chartProperties.daysLabel) || 'days'}
-                  </td>
-                  <td />
-                  <td className="time-unit">
-                    {(chartProperties && chartProperties.hoursLabel) || 'hours'}
-                  </td>
-                  <td />
-                  <td className="time-unit">
-                    {(chartProperties && chartProperties.minutesLabel) || 'minutes'}
-                  </td>
+                  {createHeaders(parts)}
                 </tr>
               </tbody>
             </table>
