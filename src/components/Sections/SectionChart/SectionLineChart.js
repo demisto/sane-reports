32297197505024b8db20ffae011d64dd3eeca925
yyosilevich <yyosilevich@paloaltonetworks.com>
2021-04-11 19:08:14 +0300

    add support for section-force-full line chart

--- src/components/Sections/SectionChart/SectionLineChart.js
+++ src/components/Sections/SectionChart/SectionLineChart.js
@@ -9,6 +9,7 @@ import moment from 'moment';
 import { NONE_VALUE_DEFAULT_NAME, QUERIES_TIME_FORMAT, SUPPORTED_TIME_FRAMES } from '../../../constants/Constants';
 import { compareFields } from '../../../utils/sort';
 import { getGraphColorByName } from '../../../utils/colors';
+import classNames from 'classnames';
 
 const SINGLE_LINE_CHART_NAME = 'sum';
 
@@ -124,8 +125,12 @@ const SectionLineChart = ({ data, style, dimensions, legend, chartProperties = {
     });
   }
 
+  const mainClass = classNames('section-line-chart', {
+    'section-force-full': chartProperties.forceFullChart === true || chartProperties.forceFullChart === undefined
+  });
+
   return (
-    <div className="section-line-chart" style={style}>
+    <div className={mainClass} style={style}>
       <AutoSizer>
         {({ width, height }) => {
           return (
