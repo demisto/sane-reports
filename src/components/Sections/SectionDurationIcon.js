5d4301d5b18920f46ef6ef1e8be4f8f95fb11c0e
yonatan-roth <yroth@paloaltonetworks.com>
2021-06-13 16:00:32 +0300

    fix unit on number

--- src/components/Sections/SectionDurationIcon.js
+++ src/components/Sections/SectionDurationIcon.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const SectionDurationIcon = ({ color = 'inherit', size = '24' }) => {
+const SectionDurationIcon = ({ color = 'inherit', size = '24px' }) => {
   return (
     <svg width={size} height={size} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
       <path
