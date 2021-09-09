a1bc87a0435a853fba5fdb4877a8f28f0829fd3b
Liron Michalevich <lmichalevich@paloaltonetworks.com>
2021-09-09 17:11:23 +0300

    style cards

--- src/components/Sections/ItemsSection.js
+++ src/components/Sections/ItemsSection.js
@@ -1,4 +1,5 @@
 import './ItemsSection.less';
+import classNames from 'classnames';
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { SECTION_ITEMS_DISPLAY_LAYOUTS, SECTION_ITEM_TYPE } from '../../constants/Constants';
@@ -114,6 +115,7 @@ class ItemsSection extends Component {
       <AutoSizer disableHeight>
         {({ width }) => {
           const columnWidth = width / columns;
+          const maxRow = maxBy(items, item => item.index).index;
           return (
             <div className="items-section" style={{ width, height: maxOffset, ...style }}>
               {title && <div className="section-title" style={titleStyle}>{title}</div>}
@@ -132,7 +134,14 @@ class ItemsSection extends Component {
               {(items || []).map((item) => {
                 const colSpan = this.getItemColSpan(item);
                 const id = this.getSectionItemKey(item);
-                const mainClass = `section-item ${(item.displayType || '').toLowerCase()}`;
+                const displayTypeClass = (item.displayType || '').toLowerCase();
+                const isCard = displayTypeClass === SECTION_ITEMS_DISPLAY_LAYOUTS.card;
+                const mainClass = classNames('section-item', {
+                  [displayTypeClass]: true,
+                  'first-column': isCard && item.startCol === 0,
+                  'last-column': isCard && item.endCol === columns,
+                  'last-row': isCard && item.index === maxRow
+                });
                 const applyStyle = {
                   transform:
                     `translate(${item.startCol * columnWidth}px, ${ItemsSection.getHeightOffset(columnUsage, item)}px)`,
@@ -144,7 +153,7 @@ class ItemsSection extends Component {
                 if (type === SECTION_ITEM_TYPE.html) {
                   dataDisplay = <SectionHTML text={item.data} />;
                 } else if (type === SECTION_ITEM_TYPE.tagsSelect) {
-                  dataDisplay = <SectionTags tags={item.data} />
+                  dataDisplay = <SectionTags tags={item.data} />;
                 }
 
                 return (
