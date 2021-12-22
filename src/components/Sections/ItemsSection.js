7fd07b3c27ba90ed55320f320b08ea7697822dd7
iizicovich <iizicovich@paloaltonetworks.com>
2021-12-22 13:06:39 +0200

    clean whitespace

--- src/components/Sections/ItemsSection.js
+++ src/components/Sections/ItemsSection.js
@@ -201,7 +201,6 @@ class ItemsSection extends Component {
                   width: colSpan * columnWidth
                 };
 
-
                 const dataDisplay = this.getDisplayDataComponent(item);
 
                 return (
