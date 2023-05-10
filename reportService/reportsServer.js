f7da9d4d5739fc8e2e234c3c18cfc64106ec3d47
Dudu Binyamin <dbinyamin@paloaltonetworks.com>
2023-05-10 18:54:50 +0300

    fix arg bool

--- reportService/reportsServer.js
+++ reportService/reportsServer.js
@@ -73,7 +73,7 @@ const MIN_TOP_MARGIN_PX = 40;
   const forceAutoHeightLayout = process.argv[14] === true || process.argv[14] === "true";
   const markdownArtifactsServerAddress = process.argv[15] || '';
   const maxTableTextLength = process.argv[16] || 300;
-  const useServerFormattedDate = process.argv[17] || "false"
+  const useServerFormattedDate =  process.argv[17] === true || process.argv[17] === "true";
   let browser;
 
   if (process.env.NODE_ENV === 'test') {
